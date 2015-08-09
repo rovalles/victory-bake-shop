a0fbd60201a3c9e1c2b16441f53d35a5311db75d
brandon morrill <brandonmorrill@gmail.com>
2015-08-09 17:49:49 -0500

    switched to old site

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+ 	$to = 'crissy@victorybakeshop.com';
+ 	$headers = 'From: ' . filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+ 	$subject = 'Inquiry From victorybakeshop.com!';
+ 	$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+ 	$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+ 	$message = $message + '\nDate Needed:\n' + filter_var($_POST['due-date'], FILTER_SANITIZE_STRING)
+
+ 	if ($_POST['extra']) {
+ 		return false;
+ 	} else {
+ 		mail($to, $subject, $message, $headers);
+ 		echo('Message sent. Thanks for contacting me. I\'ll be in touch soon!');
+ 		return true;
+ 	}
+?>
